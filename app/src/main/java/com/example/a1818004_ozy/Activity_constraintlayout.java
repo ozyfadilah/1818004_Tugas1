4488f3e2fb3d92fbd14cb1f8dbf8f8eba64fd491
ozy fadilah <ahmadozy77@gmail.com>
2021-02-03 09:20:44 +0700

    first commit

---
+++ app/src/main/java/com/example/a1818004_ozy/Activity_constraintlayout.java
@@ -0,0 +1,14 @@
+package com.example.a1818004_ozy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_constraintlayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraintlayout);
+    }
+}
